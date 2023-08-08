7b8cfd5fd7483d04f7949cce5f25bac5893a667a
smirnova.kr <smir.karina15@gmail.com>
2023-08-08 18:16:58 +0300

    Изменены entry crud

--- src/crud/entry.py
+++ src/crud/entry.py
@@ -1,9 +1,9 @@
+from uuid import UUID
+from typing import Union, Optional, List
+
 from sqlalchemy import update, and_, select
 from sqlalchemy.ext.asyncio import AsyncSession
 from datetime import datetime
-
-from uuid import UUID
-from typing import Union
 
 from db.models import Entry
 from crud.base_classes import CrudBase
@@ -27,22 +27,23 @@ class EntryDAL(
             user_agent=user_agent,
             refresh_token=refresh_token,
         )
-        self.db_session.add(new_entry)  # добавление в сессию нового пользователя
-        await self.db_session.flush()  # добавление в Постгресс нового пользователя
-        # сюда позже можно добавить проверки на существование такого пользователя
+        self.db_session.add(new_entry)
+        await self.db_session.commit()
         return new_entry
 
-    async def delete(self, uuid: Union[str, UUID]) -> Union[UUID, None]:
+    #  where(and_(Entry.uuid == id, Entry.is_active == True)). \
+    async def delete(self, id: Union[str, UUID]) -> Union[UUID, None]:
         query = update(Entry). \
-            where(and_(Entry.uuid == uuid, Entry.is_active == True)). \
+            where(Entry.uuid == id). \
             values(is_active=False).returning(Entry.uuid)
         res = await self.db_session.execute(query)
+        await self.db_session.commit()
         deleted_entry_id_row = res.fetchone()
         if deleted_entry_id_row is not None:
             return deleted_entry_id_row[0]
 
-    async def get(self, uuid: UUID) -> Union[Entry, None]:
-        query = select(Entry).where(Entry.uuid == uuid)
+    async def get(self, id: UUID) -> Union[Entry, None]:
+        query = select(Entry).where(Entry.uuid == id)
         res = await self.db_session.execute(query)
         entry_row = res.fetchone()
         if entry_row is not None:
@@ -50,17 +51,30 @@ class EntryDAL(
 
     async def update(self, uuid: UUID, **kwargs) -> Union[UUID, None]:
         query = update(Entry). \
-            where(and_(Entry.uuid == uuid, Entry.is_active == True)). \
+            where(Entry.uuid == id). \
             values(kwargs). \
             returning(Entry.uuid)
         res = await self.db_session.execute(query)
+        await self.db_session.commit()
         update_entry_id_row = res.fetchone()
         if update_entry_id_row is not None:
             return update_entry_id_row[0]
 
-    async def get_by_user_id(self, user_id: UUID) -> Union[Entry, None]:
+    async def get_by_user_id(self, user_id: UUID, unique: bool = False, only_active: bool = False) -> Optional[List[Entry]]:
         query = select(Entry).where(Entry.user_id == user_id)
+        if only_active:
+            query = query.where(Entry.is_active == True)
+        if unique:
+            query = query.distinct(Entry.user_agent)
         res = await self.db_session.execute(query)
-        entry_list = res.fetchall()
-        if entry_list is not None:
-            return entry_list
+        entries = res.scalars().fetchall()
+        return entries
+    
+    async def get_by_user_agent(self, user_agent: str, only_active: bool = False) -> Optional[Entry]:
+        query = select(Entry).where(Entry.user_agent == user_agent)
+        if only_active:
+            query = query.where(Entry.is_active == True)
+        res = await self.db_session.execute(query)
+        entry_row = res.fetchone()
+        if entry_row is not None:
+            return entry_row[0]
