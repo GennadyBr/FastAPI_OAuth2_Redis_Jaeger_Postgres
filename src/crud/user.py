15a6dca3de3b15caeb9b61a28ccae46bb83c2f11
smirnova.kr <smir.karina15@gmail.com>
2023-08-08 18:17:32 +0300

    Изменены user crud

--- src/crud/user.py
+++ src/crud/user.py
@@ -1,8 +1,8 @@
-from sqlalchemy import update, and_, select
-from sqlalchemy.ext.asyncio import AsyncSession
-
 from uuid import UUID
 from typing import Union
+
+from sqlalchemy import update, and_, select
+from sqlalchemy.ext.asyncio import AsyncSession
 
 from db.models import User
 from crud.base_classes import CrudBase
@@ -12,8 +12,7 @@ from crud.base_classes import CrudBase
 # BLOCK FOR INTERACTION WITH DATABASE IN BUSINESS CONTEXT #
 ###########################################################
 
-class UserDAL(
-    CrudBase):  # User Data Access Layer создание, удаление и все остальные функции взаимодействия с пользователем
+class UserDAL(CrudBase):  # User Data Access Layer создание, удаление и все остальные функции взаимодействия с пользователем
     """Data Access Layer for operation user CRUD"""
 
     def __init__(self, db_session: AsyncSession):
@@ -22,7 +21,6 @@ class UserDAL(
     async def create(
             self, name: str, surname: str, login: str, email: str, password: str) -> User:
         """Create User"""
-        # сюда позже добавить хеширование паролей
         new_user = User(
             name=name,
             surname=surname,
@@ -30,33 +28,35 @@ class UserDAL(
             email=email,
             password=password
         )
-        self.db_session.add(new_user)  # добавление в сессию нового пользователя
-        await self.db_session.flush()  # добавление в Постгресс нового пользователя
-        # сюда позже можно добавить проверки на существование такого пользователя
+        self.db_session.add(new_user)
+        await self.db_session.commit()
         return new_user
 
     async def delete(self, uuid: Union[str, UUID]) -> Union[UUID, None]:
         query = update(User). \
-            where(and_(User.uuid == uuid, User.is_active == True)). \
+            where(and_(User.uuid == id, User.is_active == True)). \
             values(is_active=False).returning(User.uuid)
         res = await self.db_session.execute(query)
+        await self.db_session.commit()
         deleted_user_id_row = res.fetchone()
         if deleted_user_id_row is not None:
             return deleted_user_id_row[0]
 
-    async def get(self, uuid: UUID) -> Union[User, None]:
-        query = select(User).where(User.uuid == uuid)
+    async def get(self, id: UUID) -> Union[User, None]:
+        query = select(User).where(User.uuid == id)
         res = await self.db_session.execute(query)
         user_row = res.fetchone()
         if user_row is not None:
             return user_row[0]
 
-    async def update(self, uuid: UUID, **kwargs) -> Union[UUID, None]:
+    #  where(and_(User.uuid == id, User.is_active == True)). \
+    async def update(self, id: UUID, **kwargs) -> Union[UUID, None]:
         query = update(User). \
-            where(and_(User.uuid == uuid, User.is_active == True)). \
+            where(User.uuid == id). \
             values(kwargs). \
             returning(User.uuid)
         res = await self.db_session.execute(query)
+        await self.db_session.commit()
         update_user_id_row = res.fetchone()
         if update_user_id_row is not None:
             return update_user_id_row[0]
