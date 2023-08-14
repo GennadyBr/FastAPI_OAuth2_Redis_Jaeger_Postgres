9e7e95085f7b1ca41fd48cf9895c77ff22a82ef3
GennadyBr <gennady.budazhapov@gmail.com>
2023-08-14 16:45:50 +0300

    fix comments

--- src/create_superuser.py
+++ src/create_superuser.py
@@ -3,54 +3,58 @@ import psycopg2
 from psycopg2 import Error
 import uuid
 import os
-from dotenv import load_dotenv
+# from dotenv import load_dotenv
 import bcrypt
+from core.config import user_db_settings
 
-load_dotenv()
+# load_dotenv()
 
-# создаем флаги для ввода аргументов из консоли
-parser = argparse.ArgumentParser(description="Example of a single flag acting as a boolean and an option.")
-parser.add_argument('--name', default="admin", type=str)
-parser.add_argument('--surname', default="admin", type=str)
-parser.add_argument('--login', default="admin", type=str)
-parser.add_argument('--email', type=str)
-parser.add_argument('--password', type=str)
-args = parser.parse_args()
+if __name__ == '__main__':
+    SUPER_USER = 'super_user'
 
-try:
-    # Подключиться к существующей базе данных
-    with psycopg2.connect(user=os.environ['PG_DB_USER'],
-                                  password=os.environ['PG_DB_PASSWORD'],
-                                  host=os.environ['PG_DB_HOST'],
-                                  port=os.environ['PG_DB_PORT'],
-                                  database="postgres") as connection:
+    # создаем флаги для ввода аргументов из консоли
+    parser = argparse.ArgumentParser(description="Creation super_user of user table and user_role")
+    parser.add_argument('--name', default="admin", type=str, help="Put name of super_user, ex: admin")
+    parser.add_argument('--surname', default="admin", type=str, help="Put surname of super_user, ex: admin")
+    parser.add_argument('--login', default="admin", type=str, help="Put login of super_user, ex: admin")
+    parser.add_argument('--email', type=str, help="Put login of super_user, ex: admin@gmail.com")
+    parser.add_argument('--password', type=str, help="Put password of super_user")
+    args = parser.parse_args()
 
-      cursor = connection.cursor()
+    try:
+        # Подключиться к существующей базе данных
+        with psycopg2.connect(user=user_db_settings.user,
+                                      password=user_db_settings.password.get_secret_value(),
+                                      host=user_db_settings.service_name,
+                                      port=user_db_settings.port,
+                                      database=user_db_settings.name) as connection:
 
-      salt = bcrypt.gensalt()
-      pwd_hash = bcrypt.hashpw(args.password.encode('utf-8'), salt)
+            cursor = connection.cursor()
 
-      # добавление пользователя в таблицу user
-      query = """ INSERT INTO users (uuid, name, surname, login, email, is_active, password) VALUES (%s,%s,%s,%s,%s,%s,%s)"""
-      user_id = str(uuid.uuid4())
-      params = (user_id, args.name, args.surname, args.login, args.email, True, pwd_hash)
-      cursor.execute(query, params)
+            salt = bcrypt.gensalt()
+            pwd_hash = bcrypt.hashpw(args.password.encode('utf-8'), salt)
 
-      # получение ID super_user из базы role
-      query = "select uuid from role where name='super_user'"
-      cursor.execute(query)
-      role_id = str(cursor.fetchone()[0])
+            # добавление пользователя в таблицу user
+            query = """ INSERT INTO users (uuid, name, surname, login, email, is_active, password) VALUES (%s,%s,%s,%s,%s,%s,%s)"""
+            user_id = str(uuid.uuid4())
+            params = (user_id, args.name, args.surname, args.login, args.email, True, pwd_hash)
+            cursor.execute(query, params)
 
-      # добавление пользователя в таблицу user_role
-      query = """ INSERT INTO user_role (uuid, user_id, role_id) VALUES (%s,%s,%s)"""
-      params = (str(uuid.uuid4()), user_id, role_id)
-      cursor.execute(query, params)
+            # получение ID super_user из базы role
+            query = f"""SELECT uuid FROM role WHERE name='{SUPER_USER}'"""
+            cursor.execute(query)
+            role_id = str(cursor.fetchone()[0])
 
-      connection.commit()
+            # добавление пользователя в таблицу user_role
+            query = """ INSERT INTO user_role (uuid, user_id, role_id) VALUES (%s,%s,%s)"""
+            params = (str(uuid.uuid4()), user_id, role_id)
+            cursor.execute(query, params)
 
-except (Exception, Error) as error:
-    print("Ошибка при работе с PostgreSQL", error)
-finally:
-    if connection:
-        cursor.close()
-        connection.close()
+            connection.commit()
+
+    except (Exception, Error) as error:
+        print("Ошибка при работе с PostgreSQL", error)
+    finally:
+        if connection:
+            cursor.close()
+            connection.close()
