41e21239f5f66e1b23962a5dbb4499526359b441
GennadyBr <gennady.budazhapov@gmail.com>
2023-08-14 23:24:26 +0300

    добавил crud exception

--- src/crud/user_role.py
+++ src/crud/user_role.py
@@ -4,6 +4,12 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from uuid import UUID
 from typing import List, Union
 
+import logging.config
+from core.logger import LOGGING
+
+logging.config.dictConfig(LOGGING)
+log = logging.getLogger(__name__)
+
 from db.models import UserRole
 from crud.base_classes import CrudBase
 
@@ -19,66 +25,105 @@ class UserRoleDAL(CrudBase):
         self.db_session = db_session
 
     async def create(
-            self, user_id: UUID, role_id: UUID) -> UserRole:
+            self, user_id: UUID, role_id: UUID) -> Union[UserRole, Exception]:
         """Create UserRole"""
-        new_user_role = UserRole(
-            user_id=user_id,
-            role_id=role_id
-        )
-        self.db_session.add(new_user_role)
-        await self.db_session.commit()
-        # сюда позже можно добавить проверки на существование такого пользователя
-        return new_user_role
+        try:
+            new_user_role = UserRole(
+                user_id=user_id,
+                role_id=role_id
+            )
+            self.db_session.add(new_user_role)
+            await self.db_session.commit()
+            # сюда позже можно добавить проверки на существование такого пользователя
+            return new_user_role
+        except Exception as err:
+            log.error("Ошибка: ", err)
+            log.error("Тип ошибки: ", type(err))
+            return err
 
     # удаление записи по uuid
-    async def delete(self, uuid: Union[str, UUID]) -> Union[UUID, None]:
-        user_role = await self.db_session.get(UserRole, uuid)
-        await self.db_session.delete(user_role)
-        await self.db_session.commit()
-        return user_role.uuid
+    async def delete(self, uuid: Union[str, UUID]) -> Union[UUID, None, Exception]:
+        try:
+            user_role = await self.db_session.get(UserRole, uuid)
+            await self.db_session.delete(user_role)
+            await self.db_session.commit()
+            return user_role.uuid
+        except Exception as err:
+            log.error("Ошибка: ", err)
+            log.error("Тип ошибки: ", type(err))
+            return err
 
     # удаление записи по user_id предполагается что эта функция будет вызываться в цикле перебора по user_id
-    async def delete_by_user_id(self, user_id: Union[str, UUID]) -> Union[UUID, None]:
-        user_role = await self.db_session.get(UserRole, user_id)
-        await self.db_session.delete(user_role)
-        await self.db_session.commit()
-        return user_role.uuid
+    async def delete_by_user_id(self, user_id: Union[str, UUID]) -> Union[UUID, None, Exception]:
+        try:
+            user_role = await self.db_session.get(UserRole, user_id)
+            await self.db_session.delete(user_role)
+            await self.db_session.commit()
+            return user_role.uuid
+        except Exception as err:
+            log.error("Ошибка: ", err)
+            log.error("Тип ошибки: ", type(err))
+            return err
 
     # удаление записи по role_id предполагается что эта функция будет вызываться в цикле перебора по role_id
-    async def delete_by_role_id(self, role_id: Union[str, UUID]) -> Union[UUID, None]:
-        user_role = await self.db_session.get(UserRole, role_id)
-        await self.db_session.delete(user_role)
-        await self.db_session.commit()
-        return user_role.uuid
+    async def delete_by_role_id(self, role_id: Union[str, UUID]) -> Union[UUID, None, Exception]:
+        try:
+            user_role = await self.db_session.get(UserRole, role_id)
+            await self.db_session.delete(user_role)
+            await self.db_session.commit()
+            return user_role.uuid
+        except Exception as err:
+            log.error("Ошибка: ", err)
+            log.error("Тип ошибки: ", type(err))
+            return err
 
-    async def get(self, uuid: UUID) -> Union[UserRole, None]:
-        query = select(UserRole).where(UserRole.uuid == uuid)
-        res = await self.db_session.execute(query)
-        user_role_row = res.fetchone()
-        if user_role_row is not None:
-            return user_role_row[0]
+    async def get(self, uuid: UUID) -> Union[UserRole, None, Exception]:
+        try:
+            query = select(UserRole).where(UserRole.uuid == uuid)
+            res = await self.db_session.execute(query)
+            user_role_row = res.fetchone()
+            if user_role_row is not None:
+                return user_role_row[0]
+        except Exception as err:
+            log.error("Ошибка: ", err)
+            log.error("Тип ошибки: ", type(err))
+            return err
 
-    async def get_by_user_id(self, user_id: UUID) -> List[UserRole] | None:
-        query = select(UserRole).where(UserRole.user_id == user_id)
-        res = await self.db_session.execute(query)
-        user_role_rows = res.fetchall()
-        if user_role_rows is not None:
-            return [row[0] for row in user_role_rows]
+    async def get_by_user_id(self, user_id: UUID) -> Union[List[UserRole], None, Exception]:
+        try:
+            query = select(UserRole).where(UserRole.user_id == user_id)
+            res = await self.db_session.execute(query)
+            user_role_rows = res.fetchall()
+            if user_role_rows is not None:
+                return [row[0] for row in user_role_rows]
+        except Exception as err:
+            log.error("Ошибка: ", err)
+            log.error("Тип ошибки: ", type(err))
+            return err
 
-    async def get_by_role_id(self, role_id: UUID) -> Union[UserRole, None]:
-        # не будет работать в таком виде. Но пока нигде не используется
-        query = select(UserRole).where(UserRole.role_id == role_id)
-        res = await self.db_session.execute(query)
-        user_role_row = res.fetchone()
-        if user_role_row is not None:
-            return user_role_row[0]
+    async def get_by_role_id(self, role_id: UUID) -> Union[List[UserRole], None, Exception]:
+        try:
+            query = select(UserRole).where(UserRole.role_id == role_id)
+            res = await self.db_session.execute(query)
+            user_role_rows = res.fetchall()
+            if user_role_rows is not None:
+                return [row[0] for row in user_role_rows]
+        except Exception as err:
+            log.error("Ошибка: ", err)
+            log.error("Тип ошибки: ", type(err))
+            return err
 
-    async def update(self, uuid: UUID, **kwargs) -> Union[UUID, None]:
-        query = update(UserRole). \
-            where(UserRole.uuid == uuid). \
-            values(kwargs). \
-            returning(UserRole.uuid)
-        res = await self.db_session.execute(query)
-        update_user_role_id_row = res.fetchone()
-        if update_user_role_id_row is not None:
-            return update_user_role_id_row[0]
+    async def update(self, uuid: UUID, **kwargs) -> Union[UUID, None, Exception]:
+        try:
+            query = update(UserRole). \
+                where(UserRole.uuid == uuid). \
+                values(kwargs). \
+                returning(UserRole.uuid)
+            res = await self.db_session.execute(query)
+            update_user_role_id_row = res.fetchone()
+            if update_user_role_id_row is not None:
+                return update_user_role_id_row[0]
+        except Exception as err:
+            log.error("Ошибка: ", err)
+            log.error("Тип ошибки: ", type(err))
+            return err
