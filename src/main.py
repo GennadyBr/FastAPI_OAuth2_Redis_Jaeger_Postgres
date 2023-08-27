ff6e023c5544cc2c3462c4ccbd4dd131e0f190cb
GennadyBr <gennady.budazhapov@gmail.com>
2023-08-27 19:00:59 +0300

    добавлен middleware и логи

--- src/main.py
+++ src/main.py
@@ -12,9 +12,14 @@ from opentelemetry.exporter.jaeger.thrift import JaegerExporter
 
 from api.v1.roles import router as role_router
 from api.v1.auth import router as auth_router
+import logging.config
 from core.logger import LOGGING
 from utils.limits import check_limit
 from core.config import app_settings, jaeger_settings
+
+logging.config.dictConfig(LOGGING)
+log = logging.getLogger(__name__)
+
 
 def configure_tracer() -> None:
     """
@@ -33,7 +38,8 @@ def configure_tracer() -> None:
     # Чтобы видеть трейсы в консоли
     trace.get_tracer_provider().add_span_processor(BatchSpanProcessor(ConsoleSpanExporter()))
 
-configure_tracer() #Jaeger instrument for tracer, must be before app = FastAPI
+
+configure_tracer()  # Jaeger instrument for tracer, must be before app = FastAPI
 
 app = FastAPI(
     title=app_settings.project_name,
@@ -45,9 +51,10 @@ app = FastAPI(
     default_response_class=ORJSONResponse,
 )
 
-FastAPIInstrumentor.instrument_app(app) #Jaeger instrument for tracer, must be after app = FastAPI
+FastAPIInstrumentor.instrument_app(app)  # Jaeger instrument for tracer, must be after app = FastAPI
 
-@app.middleware("http") #добавлено через rebase from dev 2023-08-27 at 16-29
+
+@app.middleware("http")
 async def before_request(request: Request, call_next):
     user_id = request.headers.get("X-Forwarded-For")
     result = await check_limit(user_id=user_id)
@@ -58,23 +65,22 @@ async def before_request(request: Request, call_next):
         )
     return await call_next(request)
 
+
 @app.middleware('http')
 async def before_request(request: Request, call_next):
     """
-    Обработчик запрета выполнять запросы без заголовка X-Request-Id.
-    Гарантирует, что в случае сбоя вы сможете провести аудит запроса пользователя.
+    Обработчик заголовка X-Request-Id.
     """
     response = await call_next(request)
-    request_id = request.headers.get('X-Request-Id')
-    if not request_id:
-        return ORJSONResponse(status_code=status.HTTP_400_BAD_REQUEST, content={'detail': 'X-Request-Id is required'})
+    log.info(f'<<<request_id middleware>>>')
+    request_id = request.headers.get('X-Request-Id', None)
+    log_msg = f'{request_id=}'
+    log.debug(log_msg)
     return response
-
 
 
 app.include_router(auth_router, prefix='/api/v1', tags=['auth'])
 app.include_router(role_router, prefix='/api/v1', tags=['role'])
-
 
 if __name__ == '__main__':
     uvicorn.run(
